04fb131cc6b4ef6abdb29c650f4ef6a9ae02502e
Rahul <rahul.somaliya@nirmanlab.com>
2022-02-25 13:35:41 +0530

    implemented echo successfully on getUser method

--- backend/app/handlers/handler.go
+++ backend/app/handlers/handler.go
@@ -3,18 +3,19 @@ package handlers
 import (
 	"apiArchitecture/business/services/user"
 	"database/sql"
-	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
 )
 
-func Api(db *sql.DB, router *mux.Router) {
+func Api(db *sql.DB, router *echo.Echo) {
 	//here, the function accepts the database pointer that we've sent, initializes the userHandler Struct by calling
 	// that New() function from service(user) package
 	h := userHandlers{
 		user: user.New(db),
 	}
 
-	router.HandleFunc("/v1/users/", h.GetUsersRequest).Methods(http.MethodGet)
+	// router.HandleFunc("/v1/users/", h.GetUsersRequest).Methods(http.MethodGet)
+	router.GET("/v1/users/", h.GetUsersRequest)
+	router.GET("/v1/users/:id/", h.GetUsersRequest)
 
 }
--- backend/app/main.go
+++ backend/app/main.go
@@ -3,32 +3,29 @@ package main
 import (
 	"apiArchitecture/app/handlers"
 	"apiArchitecture/business/database"
-	
-	"fmt"
-	"log"
-	"net/http"
 
-	muxhan "github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	_"github.com/lib/pq"
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	db := database.Setup()
 	defer db.Close()
 
-	router := mux.NewRouter()
-	
+	// router := mux.NewRouter()
+	router := echo.New()
+
 	//API FUNCTION TO BE CREATED
 	handlers.Api(db, router)
 
-	// router := echo.New()
-
 	fmt.Println("Up!")
 
-	headersOk := muxhan.AllowedHeaders([]string{"*"})
-	originsOk := muxhan.AllowedOrigins([]string{"*"})
-	methodsOk := muxhan.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS", "DELETE"})
+	// headersOk := muxhan.AllowedHeaders([]string{"*"})
+	// originsOk := muxhan.AllowedOrigins([]string{"*"})
+	// methodsOk := muxhan.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS", "DELETE"})
 
-	log.Fatal(http.ListenAndServe(":5434", muxhan.CORS(headersOk, originsOk, methodsOk)(router)))
+	// log.Fatal(http.ListenAndServe(":5434", muxhan.CORS(headersOk, originsOk, methodsOk)(router)))
+	router.Logger.Fatal(router.Start(":5435"))
 }
